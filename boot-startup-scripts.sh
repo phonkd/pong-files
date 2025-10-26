2814f5e9acff1dc7ca1af04e2d978334b0fb0813
Phonkd <enst18.12@gmail.com>
2025-10-26 23:38:37 +0100

    Update boot-startup-scripts.sh

--- boot-startup-scripts.sh
+++ boot-startup-scripts.sh
@@ -2,6 +2,11 @@
 termux-wake-lock
 sshd
 #/data/data/com.termux/files/usr/bin/sv up sshd
-/data/data/com.termux/files/usr/bin/sv up dockerd
+export SVDIR="$PREFIX/var/service"
+
+termux-services start   # ensure runsvdir is running; no effect if already started
+sleep 10                # wait for storage/network if dockerd needs them
+
+sv up dockerd
 /data/data/com.termux/files/home/pong-files/network-routes.sh
 # cp /data/data/com.termux/files/home/pong-files/boot-startup-scripts.sh /data/data/com.termux/files/home/.termux/boot/start-stuff.sh
