dd33ff890a0bcd70b286fef139aa4ba931461b22
Phonkd <enst18.12@gmail.com>
2025-10-26 22:59:04 +0100

    add stuff

---
+++ boot-startup-scripts.sh
@@ -0,0 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/sh
+sv up sshd
+sv up dockerd
+/data/data/com.termux/files/home/pong-files/network-routes.sh
+# ln -s /data/data/com.termux/files/home/pong-files/boot-startup-scripts.sh /data/data/com.termux/files/.termux/boot/start-stuff.sh
---
+++ network-routes.sh
@@ -0,0 +1,20 @@
+#!/system/bin/sh
+# This script enables the use of containers without network_mode host...
+# find the route kernel would use to reach the Internet
+LINE="$(ip -o route get 1.1.1.1 2>/dev/null || ip -o route get 8.8.8.8 2>/dev/null)"
+
+GW="$(printf '%s\n' "$LINE" | awk '{for(i=1;i<=NF;i++) if($i=="via") {print $(i+1); exit}}')"
+IF="$(printf '%s\n' "$LINE" | awk '{for(i=1;i<=NF;i++) if($i=="dev") {print $(i+1); exit}}')"
+
+# optional: clean duplicate defaults
+ip route del default 2>/dev/null
+
+if [ -n "$GW" ]; then
+  ip route replace default via "$GW" dev "$IF" metric 100
+else
+  # point-to-point (no gateway)
+  ip route replace default dev "$IF" metric 100
+fi
+
+# ensure your rule exists
+ip rule add from all lookup main pref 30000 2>/dev/null
