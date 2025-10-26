54004f61fb4f91617011f39b0d915451ae49d24f
Phonkd <enst18.12@gmail.com>
2025-10-26 23:34:00 +0100

    Update boot-startup-scripts.sh

--- boot-startup-scripts.sh
+++ boot-startup-scripts.sh
@@ -4,4 +4,4 @@ sshd
 #/data/data/com.termux/files/usr/bin/sv up sshd
 /data/data/com.termux/files/usr/bin/sv up dockerd
 /data/data/com.termux/files/home/pong-files/network-routes.sh
-# ln -s /data/data/com.termux/files/home/pong-files/boot-startup-scripts.sh /data/data/com.termux/files/home/.termux/boot/start-stuff.sh
+# cp /data/data/com.termux/files/home/pong-files/boot-startup-scripts.sh /data/data/com.termux/files/home/.termux/boot/start-stuff.sh
